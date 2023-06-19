3bfc0f9a3c3dbbac98779a078568a7f259033cfe
Maxim Latnikov <latnikou@gmail.com>
2023-06-19 15:00:25 +0300

    changed interface task solution

--- interface/script.js
+++ interface/script.js
@@ -4,13 +4,16 @@ const makePoint = (x, y) => {
   const point = {
     angle: Math.atan2(y, x),
     radius: Math.sqrt((x ** 2) + (y ** 2)),
-    points: [x, y],
   };
+
   return point;
 };
+
 // BEGIN (write your solution here)
-const getX = (point) => point.points[0];
-const getY = (point) => point.points[1];
+const getAngle = (point) => point.angle;
+const getRadius = (point) => point.radius;
+const getX = (point) => Math.round(getRadius(point) * Math.cos(getAngle(point)));
+const getY = (point) => Math.round(getRadius(point) * Math.sin(getAngle(point)));
 // END
 
 export { makePoint, getX, getY };
