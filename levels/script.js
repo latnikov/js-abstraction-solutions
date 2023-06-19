530936d9f46fba423349f0a30780257e0ad6c19f
Maxim Latnikov <latnikou@gmail.com>
2023-06-19 15:49:24 +0300

    solved levels task

---
+++ levels/script.js
@@ -0,0 +1,30 @@
+// @ts-check
+// eslint-disable-next-line
+import { makeDecartPoint, getX, getY, getQuadrant } from './points.js';
+
+// BEGIN (write your solution here)
+const makeRectangle = (point, width, height) => ({ point, width, height });
+
+const getStartPoint = (rectangle) => rectangle.point;
+const getWidth = (rectangle) => rectangle.width;
+const getHeight = (rectangle) => rectangle.height;
+
+const containsOrigin = (rectangle) => {
+  const point1 = getStartPoint(rectangle);
+  const point2 = makeDecartPoint(
+    getX(point1) + getWidth(rectangle),
+    getY(point1) - getHeight(rectangle),
+  );
+
+  return getQuadrant(point1) === 2 && getQuadrant(point2) === 4;
+};
+
+
+export {
+  makeRectangle,
+  containsOrigin,
+  getStartPoint,
+  getWidth,
+  getHeight,
+};
+// END
